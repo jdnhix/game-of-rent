64df45f1cbe6423cceef5bac667b661f5c08f8f2
jdnhix <jdhix98890@yahoo.com>
2020-01-06 09:04:03 -0600

    Map init

---
+++ src/components/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Map.css';
+
+function App() {
+    return (
+        <div className="Map">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    );
+}
+
+export default App;
